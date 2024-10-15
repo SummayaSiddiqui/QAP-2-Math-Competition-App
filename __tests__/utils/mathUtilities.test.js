131cdf64b249ad22291f2ee2b55da27946aecbd4
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2024-10-15 19:29:33 -0230

    Updated mathUtilities.test.js

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -1,9 +1,32 @@
-const { isCorrectAnswer, getQuestion } = require("../../utils/mathUtilities");
+const {
+  isCorrectAnswer,
+  getQuestion,
+} = require("../../utils/mathUtilities");
 
 describe("Tests for getQuestion", () => {
+  test("should generate a valid math question with two numbers and an operator", () => {
+    const { question, answer } = getQuestion();
 
+    expect(question).toMatch(/\d+ [+-] \d+/);
+
+    expect(typeof answer).toBe("number");
+  });
 });
 
 describe("Tests for isCorrectAnswer", () => {
-    
+  test("should return true for a correct answer", () => {
+    const { question, answer } = getQuestion();
+    const result = isCorrectAnswer(question, answer);
+
+    expect(result).toBe(true);
+  });
+
+  test("should return false for an incorrect answer", () => {
+    const { question } = getQuestion();
+    const incorrectAnswer = -1;
+
+    const result = isCorrectAnswer(question, incorrectAnswer);
+
+    expect(result).toBe(false);
+  });
 });
